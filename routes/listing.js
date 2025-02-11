5d02d26d33b6c8f646871fc5461c0def3057d41f
Vicky Kumar <someone@gmail.com>
2025-02-11 22:28:18 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const router=express.Router();
+const Listing=require("../models/listing.js");
+const wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/ExpressError.js");
+const {listingSchema,reviewSchema}=require("../schema.js");
+
+const { isLoggedIn,isOwner,validateListing }=require("../middleware.js");
+
+const listingController=require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn,upload.single('listing[image]'),validateListing,wrapAsync(listingController.createListing))
+
+    // .post(upload.single('listing[image]'),(req,res)=>{
+    //     res.send(req.file);
+    // })
+    
+
+
+router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn,isOwner,upload.single('listing[image]'),validateListing,wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+
+    
+router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
+
+
+
+module.exports=router;
